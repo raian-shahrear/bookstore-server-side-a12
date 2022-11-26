c6824662c0df609246828f2609939b4418743616
raian-shahrear <raianshahrearbd@gmail.com>
2022-11-27 03:37:36 +0600

    get categories from MDB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,7 +13,26 @@ app.use(express.json());
 
 
 
+// MongoDB database
+const uri = `mongodb+srv://${process.env.MDB_USER_NAME}:${process.env.MDB_USER_PASS}@cluster0.xnvdy5u.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+  try{
+    // MDB collections
+    const categoriesCollection = client.db("book-store").collection("categories");
+
+
+    // get all categories
+    app.get('/categories', async(req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.send(categories);
+    })
+  }
+  finally{}
+}
+run().catch(err => console.log(err))
 
 
 
